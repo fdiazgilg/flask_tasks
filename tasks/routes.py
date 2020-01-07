609f5b65dbe4b2de1fea82106cdc604fd681d1cf
fdiazgilg <fdiazgilg@gmail.com>
2020-01-08 00:56:48 +0100

    Versión Web con Formulario

--- tasks/routes.py
+++ tasks/routes.py
@@ -1,17 +1,22 @@
 from tasks import app
 from flask import render_template, request, redirect, url_for
+from tasks.forms import TaskForm
 
 import csv
+
+DATOS = './data/tareas.dat'
 
 @app.route("/")
 def index():
     #leer el fichero csv
-    fTasks = open('./data/tareas.dat', 'r')
+    fTasks = open(DATOS, 'r')
     csvreader = csv.reader(fTasks, delimiter=',')
     datos = []
 
     for linea in csvreader:
         datos.append(linea)
+
+    fTasks.close()
    
     if datos != []:
         datos.sort(key=lambda x: x[2])
@@ -21,23 +26,27 @@ def index():
 
 @app.route("/newtask", methods=['GET', 'POST'])
 def newtask():
+    form = TaskForm(request.form)
+
     if request.method == 'GET':
-        return render_template("task.html")
+        return render_template("task.html", form=form)
     
-    if request.method == 'POST':
-   
-        fTasks = open('./data/tareas.dat', 'a')
+    if form.validate():
+        fTasks = open(DATOS, 'a')
         csvwriter = csv.writer(fTasks, delimiter=",", quotechar='"', lineterminator='\r')
 
         title = request.values.get('title')
-        desc = request.values.get('desc')
+        desc = request.values.get('description')
         date = request.values.get('date')
 
         csvwriter.writerow([title, desc, date])
 
-        fTasks.close
+        fTasks.close()
 
         return redirect(url_for("index"))
+
+    else:
+        return render_template("task.html", form=form)
 
     print('method:', request.method)
     print('parametros:', request.values)
