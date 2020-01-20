547d48c4cbb274f4554f29947fc449c484161cd2
fdiazgilg <fdiazgilg@gmail.com>
2020-01-20 12:44:16 +0100

    Fichero sql corregido

--- migrations/2020011601creatablas.sql
+++ migrations/2020011601creatablas.sql
@@ -7,7 +7,7 @@ CREATE TABLE "tareas1" (
 	"fecha"	TEXT NOT NULL,
 	"id_empleado"	INTEGER,
 	FOREIGN KEY("id_empleado") REFERENCES "empleados"("id")
-)
+);
 
 INSERT INTO "tareas1" (titulo, descripcion, fecha) VALUES (SELECT titulo, descripcion, fecha FROM tareas);
 
